65e9a99bc3c5eb859df45e5a842770691a928545
Abhay <abhay@keyvalue.systems>
2023-07-04 16:17:31 +0530

    fix: createImagesToPdf function refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,65 +1,29 @@
-import {
-  ColorValue,
-  NativeModules,
-  processColor,
-  ProcessedColorValue,
-} from 'react-native';
+import { NativeModules } from 'react-native';
 const { ImageProcessingSDK } = NativeModules;
-
-export function isImageBlurred(imageUrl: string): Promise<boolean> {
-  return ImageProcessingSDK.isImageBlurred(imageUrl);
-}
-
-export function createImagesToPdf(options: CreatePdfOptions): Promise<string> {
-  return createPdf(options);
-}
 
 export type ImageFit = 'none' | 'fill' | 'contain' | 'cover';
 
 export type Page = {
   imagePath: string;
   imageFit?: ImageFit;
-  width?: number;
-  height?: number;
-  backgroundColor?: ColorValue;
 };
 
-interface InternalPage extends Omit<Page, 'backgroundColor'> {
-  backgroundColor?: ProcessedColorValue;
+export type CreatePdfOptions = {
+  outputDirectory?: string;
+  outputFilename?: string;
+  images: Array<string>;
+};
+
+export function isImageBlurred(imageUrl: string): Promise<boolean> {
+  return ImageProcessingSDK.isImageBlurred(imageUrl);
 }
 
-export type CreatePdfOptions =
-  | {
-      outputDirectory?: string;
-      outputFilename?: string;
-      images: Array<Page | string>;
-      imagePaths?: undefined;
-    }
-  | {
-      outputDirectory?: string;
-      outputFilename?: string;
-      images?: undefined;
-      /**
-       * @deprecated Use the `pages` property instead.
-       */
-      imagePaths: string[];
-    };
+export function createImagesToPdf(options: CreatePdfOptions): Promise<string> {
+  const { images, ...opts } = options;
 
-function createPdf(options: CreatePdfOptions): Promise<string> {
-  const { images, imagePaths, ...opts } = options;
-
-  const internalPages = (imagePaths || images || []).map<InternalPage>((e) => {
-    if (typeof e === 'string') {
-      return {
-        imagePath: e,
-      };
-    }
-
-    const { backgroundColor, ...page } = e;
-
+  const internalPages = images?.map((url) => {
     return {
-      backgroundColor: processColor(backgroundColor) ?? undefined,
-      ...page,
+      imagePath: url,
     };
   });
 
