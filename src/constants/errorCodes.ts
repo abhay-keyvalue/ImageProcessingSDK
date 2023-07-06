854ec85fdc395ab5264df207563ff8c382dbdb70
nikita-keyvalue <nikita@keyvalue.systems>
2023-07-06 16:25:31 +0530

    Add error codes and README.md

---
+++ src/constants/errorCodes.ts
@@ -0,0 +1,15 @@
+export const ERROR_CODES = {
+    ERR001: {
+      code: 'ERR001',
+      message: 'Invalid url or image path',
+    },
+    ERR002: {
+      code: 'ERR002',
+      message: 'Unsupported Media Types',
+    },
+    ERR003: {
+      code: 'ERR003',
+      message: 'File Exceeded Max Size',
+    }
+   
+  };
