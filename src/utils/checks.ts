50f423933ab14775bc9d1abc546e9872410805d2
nikita-keyvalue <nikita@keyvalue.systems>
2023-07-07 14:07:59 +0530

    feat: Add error handling

--- src/utils/checks.ts
+++ src/utils/checks.ts
@@ -1,4 +1,16 @@
-export const validateImageUrl = (value: string) => {
-  console.log('value', value);
+export const validateImageUrl = (value: string): boolean => {
+  if (value === null || value.trim() === '') {
+    return false; // Null or empty string
+  }
+  return true;
+};
+
+export const validateImageExtension = (value: string): boolean => {
+// Check if the file has an image extension
+const imageExtensions = ['.jpg', '.jpeg', '.png'];
+const fileExtension = value.substring(value.lastIndexOf('.')).toLowerCase();
+if (!imageExtensions.includes(fileExtension)) {
+  return false; // Invalid image file extension
+}
   return true;
 };
